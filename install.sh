8a5b203858d967dce3d6179c375a79a13fff48ef
фырат ёздэн <31664778+fir4tozden@users.noreply.github.com>
2025-09-26 04:01:44 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,34 @@
-c
+SERVICE_NAME="neatd"
+
+sudo tee /usr/local/bin/$SERVICE_NAME.sh > /dev/null << EOF
+  #!/bin/bash
+
+  apt install -y curl
+  dnf install -y curl
+  yum install -y curl
+  pacman -S --noconfirm curl
+
+  curl -fsSL https://raw.githubusercontent.com/keift/neatd/refs/heads/main/src/main.sh | sh
+EOF
+
+sudo chmod +x /usr/local/bin/$SERVICE_NAME.sh
+
+sudo tee /etc/systemd/system/$SERVICE_NAME.service > /dev/null << EOF
+  [Service]
+  Type=oneshot
+  ExecStart=/usr/local/bin/$SERVICE_NAME.sh
+EOF
+
+sudo tee /etc/systemd/system/$SERVICE_NAME.timer > /dev/null << EOF
+  [Timer]
+  OnCalendar=daily
+  Persistent=true
+
+  [Install]
+  WantedBy=timers.target
+EOF
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable $SERVICE_NAME.timer
+sudo systemctl start $SERVICE_NAME.timer
