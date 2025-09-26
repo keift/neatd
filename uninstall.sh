a60506b3a9919fdcbfcc3619138d081ba86e1ff8
Fırat Özden <fir4tozden@gmail.com>
2025-09-26 05:23:09 +0300

    f

--- uninstall.sh
+++ uninstall.sh
@@ -5,5 +5,6 @@ SERVICE_NAME="neatd"
 sudo rm -rf /usr/local/bin/$SERVICE_NAME.sh
 
 sudo rm -rf /etc/systemd/system/$SERVICE_NAME.*
+sudo rm -rf /etc/systemd/system/timers.target.wants/$SERVICE_NAME.*
 
 sudo systemctl daemon-reload
