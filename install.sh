b87b61034380db613cfcf2b03ea7602f0d53b568
Fırat Özden <fir4tozden@gmail.com>
2025-09-27 01:25:47 +0300

    f

--- install.sh
+++ install.sh
@@ -3,32 +3,32 @@
 SERVICE_NAME="neatd"
 
 sudo tee /usr/local/bin/$SERVICE_NAME.sh > /dev/null << EOF
-  #!/bin/bash
+#!/bin/bash
 
-  apt install -y curl
-  dnf install -y curl
-  yum install -y curl
-  zypper -n install curl
-  pacman -S --noconfirm curl
+apt install -y curl
+dnf install -y curl
+yum install -y curl
+zypper -n install curl
+pacman -S --noconfirm curl
 
-  curl -fsSL https://raw.githubusercontent.com/keift/neatd/refs/heads/main/src/main.sh | sh
+curl -fsSL https://raw.githubusercontent.com/keift/neatd/refs/heads/main/src/main.sh | sh
 EOF
 
 sudo chmod +x /usr/local/bin/$SERVICE_NAME.sh
 
 sudo tee /etc/systemd/system/$SERVICE_NAME.service > /dev/null << EOF
-  [Service]
-  Type=oneshot
-  ExecStart=/usr/local/bin/$SERVICE_NAME.sh
+[Service]
+Type=oneshot
+ExecStart=/usr/local/bin/$SERVICE_NAME.sh
 EOF
 
 sudo tee /etc/systemd/system/$SERVICE_NAME.timer > /dev/null << EOF
-  [Timer]
-  OnCalendar=daily
-  Persistent=true
+[Timer]
+OnCalendar=daily
+Persistent=true
 
-  [Install]
-  WantedBy=timers.target
+[Install]
+WantedBy=timers.target
 EOF
 
 sudo systemctl daemon-reload
