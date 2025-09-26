42fafed4dd18403e2af1f3639b015f0356d7af14
Fırat Özden <fir4tozden@gmail.com>
2025-09-26 04:58:18 +0300

    f

--- src/main.sh
+++ src/main.sh
@@ -1,27 +1,27 @@
-  #!/bin/bash
+#!/bin/bash
 
-  apt install -y coreutils util-linux
-  apt install -y --reinstall coreutils util-linux
+apt install -y coreutils util-linux
+apt install -y --reinstall coreutils util-linux
 
-  dnf install -y coreutils util-linux
-  dnf reinstall -y coreutils util-linux
+dnf install -y coreutils util-linux
+dnf reinstall -y coreutils util-linux
 
-  yum install -y coreutils util-linux
-  yum reinstall -y coreutils util-linux
+yum install -y coreutils util-linux
+yum reinstall -y coreutils util-linux
 
-  pacman -S --noconfirm coreutils util-linux
+pacman -S --noconfirm coreutils util-linux
 
-  apt update -y
-  apt upgrade -y
-  apt autoremove -y
+apt update -y
+apt upgrade -y
+apt autoremove -y
 
-  dnf check-update -y
-  dnf upgrade -y
-  dnf autoremove -y
+dnf check-update -y
+dnf upgrade -y
+dnf autoremove -y
 
-  yum check-update -y
-  yum update -y
-  yum autoremove -y
+yum check-update -y
+yum update -y
+yum autoremove -y
 
-  pacman -Syu --noconfirm
-  pacman -Rns --noconfirm $(pacman -Qdtq)
+pacman -Syu --noconfirm
+pacman -Rns --noconfirm $(pacman -Qdtq)
