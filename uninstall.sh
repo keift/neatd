5b342cef40c2e04ded0eb2c2170b0ae0c37c0c8d
Fırat Özden <fir4tozden@gmail.com>
2025-09-26 21:49:23 +0300

    f

--- uninstall.sh
+++ uninstall.sh
@@ -6,6 +6,6 @@ sudo rm -rf /usr/local/bin/$SERVICE_NAME.sh
 
 sudo rm -rf /etc/systemd/system/$SERVICE_NAME.*
 sudo rm -rf /etc/systemd/system/timers.target.wants/$SERVICE_NAME.*
-sudo rm -rf /var/lib/systemd/timers/$SERVICE_NAME.*
+sudo rm -rf /var/lib/systemd/timers/stamp-$SERVICE_NAME.*
 
 sudo systemctl daemon-reload
