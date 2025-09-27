85143006ee71ce1d1a5698e194be01c5a9154f26
фырат ёздэн <31664778+fir4tozden@users.noreply.github.com>
2025-09-27 03:00:33 +0300

    Update main.sh

--- src/main.sh
+++ src/main.sh
@@ -47,3 +47,5 @@ systemctl start systemd-tmpfiles-clean.timer
 sed -i "/^127\.0\.1\.1\s\+/s/\S\+$/$(hostname)/" /etc/hosts
 
 ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+
+echo "Neatd worked successfully."
